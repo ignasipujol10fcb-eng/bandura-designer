430494a74812ba65199334d5ff7239f54de17012
ignasipujol10fcb-eng <ignasipujol10fcb@gmail.com>
2026-09-04 21:30:43 +0300

    Make real bandura image fallbacks use reliable direct Wikimedia assets

--- image-resilience.js
+++ image-resilience.js
@@ -1,17 +1,20 @@
 /* Keep the real bandura photography resilient if an external image request fails. */
 (() => {
   const fallbacks = [
-    'https://commons.wikimedia.org/wiki/Special:FilePath/Chernihiv-style_bandura.jpg?width=1200',
-    'https://commons.wikimedia.org/wiki/Special:FilePath/Chernihiv_Bandura3.jpg?width=1600'
+    'https://upload.wikimedia.org/wikipedia/commons/5/59/Chernihiv-style_bandura.jpg',
+    'https://upload.wikimedia.org/wikipedia/commons/thumb/5/52/Chernihiv_Bandura3.jpg/1280px-Chernihiv_Bandura3.jpg'
   ];
   const failed = new WeakSet();
-  document.querySelectorAll('img[src*="commons.wikimedia.org"]').forEach(img => {
+  document.querySelectorAll('img[src*="wikimedia.org"]').forEach(img => {
     img.addEventListener('error', () => {
       if (failed.has(img)) return;
       failed.add(img);
       const current = img.currentSrc || img.src;
       const next = fallbacks.find(src => src !== current);
-      if (next) img.src = next;
-    }, { once: true });
+      if (next) {
+        img.src = next;
+        img.removeAttribute('srcset');
+      }
+    });
   });
 })();
